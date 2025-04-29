69106bd2ab2bc3efa290187eaede7c172bc73e37
Giorgia Del Missier <107466296+gioodm@users.noreply.github.com>
2025-04-29 16:23:02 +0000

    update pubs

--- update_publications.py
+++ update_publications.py
@@ -2,6 +2,9 @@ import requests
 import bibtexparser
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
+import re
+import html
+import unicodedata
 
 def fetch_openalex_works(author_name):
     # Step 1: Find the author ID
@@ -36,29 +39,30 @@ def fetch_openalex_works(author_name):
 
     return works
 
+def normalize_title(title):
+    """remove HTML tags and normalize unicode."""
+    title = html.unescape(title)                         # convert HTML entities like &lt; to <
+    title = re.sub(r'<[^>]+>', '', title)                # remove HTML tags like <i>
+    title = unicodedata.normalize('NFKD', title)         # normalize accented characters
+    title = re.sub(r'\s+', ' ', title).strip()           # normalize whitespace, lowercase
+    return title
+
 def create_bibtex_entry(publication):
-
-    authors = []
-    for author in publication['authorships']:
-        authors.append(author['author']['display_name'])
-
+    authors = [a['author']['display_name'] for a in publication['authorships']]
     try: 
         journal = publication['primary_location']['source']["display_name"]
     except:
         journal = ''
-
     try:
-        if publication['doi']:
-            url = publication['doi']
-        else:
-            url = publication['primary_location']['landing_page_url']
+        url = publication['doi'] if publication['doi'] else publication['primary_location']['landing_page_url']
     except:
         url = publication['primary_location']['landing_page_url']
 
+    title = normalize_title(publication['title'])
     bib_entry = {
         "ENTRYTYPE": publication.get('type', 'article'),
         "ID": publication.get('id', 'unknown_id'),
-        "title": publication.get('title', ''),
+        "title": title,
         "author": ' and '.join(authors),
         "journal": journal,
         "year": str(publication.get('publication_year', '')),
@@ -70,15 +74,29 @@ def create_bibtex_entry(publication):
 author_name = "Rodrigo Ledesma-Amaro"
 works = fetch_openalex_works(author_name)
 
+# Deduplication logic
+seen = {}
 bib_entries = []
+
 for work in works:
-    try: 
-        work['primary_location']['landing_page_url']
-        bib_entries.append(create_bibtex_entry(work))
-    except:
+    try:
+        title_norm = normalize_title(work['title'])
+        year = str(work.get('publication_year', ''))
+        key = ','.join([a['author']['display_name'] for a in work['authorships']])
+
+        if key in seen:
+            # Prefer peer-reviewed over preprint if duplicate found
+            existing = seen[key]
+            if existing['ENTRYTYPE'] == 'preprint' and work.get('type') != 'preprint':
+                seen[key] = create_bibtex_entry(work)
+        else:
+            seen[key] = create_bibtex_entry(work)
+    except Exception as e:
         continue
 
-# Write to .bib file
+bib_entries = list(seen.values())
+
+# Write to .bib
 bib_database = BibDatabase()
 bib_database.entries = bib_entries
 writer = BibTexWriter()
